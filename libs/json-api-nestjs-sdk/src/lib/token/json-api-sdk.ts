a0cbf7a4073de9d9062063aa399b28fb006b43b8
Alexander Kharkovey <alexander.kharkovey@nordigy.ru>
2022-12-17 18:17:28 +0600

    fix(json-api-nestjs-sdk): remove dead code

--- libs/json-api-nestjs-sdk/src/lib/token/json-api-sdk.ts
+++ libs/json-api-nestjs-sdk/src/lib/token/json-api-sdk.ts
@@ -11,13 +11,6 @@ export interface ListEntities {
 
 export const JSON_API_SDK_CONFIG = new InjectionToken<JsonApiSdkConfig>(
   'Main config object for sdk'
-  // {
-  //   // providedIn: JsonApiNestjsSdkModule,
-  //   factory: () => {
-  //     console.log(inject(API_SDK_CONFIG, { optional: true }));
-  //     return inject(API_SDK_CONFIG, { optional: true }) as any;
-  //   },
-  // }
 );
 
 export const API_SDK_CONFIG = new InjectionToken<JsonApiSdkConfig>(
