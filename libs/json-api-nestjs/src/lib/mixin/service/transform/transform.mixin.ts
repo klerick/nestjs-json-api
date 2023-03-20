9df70267fa271ae9e891c2a5b0724639ad6f10b2
Mateus Garcia <mateus.garcia@motrix.global>
2023-03-20 03:35:15 -0300

    fix: reduce relationships to unique values

--- libs/json-api-nestjs/src/lib/mixin/service/transform/transform.mixin.ts
+++ libs/json-api-nestjs/src/lib/mixin/service/transform/transform.mixin.ts
@@ -186,10 +186,17 @@ export class TransformMixinService<T> {
         const propsData = data[field];
         const typeName = getEntityName(this.relationTarget.get(field));
         if (Array.isArray(propsData)) {
-          builtData.data = propsData.map((i) => ({
-            type: camelToKebab(typeName),
-            id: i[this.relationPrimaryField.get(field)].toString(),
-          }));
+          builtData.data = propsData
+            .map((i) => ({
+              type: camelToKebab(typeName),
+              id: i[this.relationPrimaryField.get(field)].toString(),
+            }))
+            .reduce((acum, i) => {
+              if (acum.find((a) => a.id === i.id)) {
+                return acum;
+              }
+              return [...acum, i];
+            }, []);
         }
 
         if (!Array.isArray(data[field]) && typeof data[field] !== 'undefined') {
