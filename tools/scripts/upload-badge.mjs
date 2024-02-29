4af9343f64f1b126b1444927c736980689c699ee
Alex H <klerick666@gmail.com>
2024-02-29 18:45:08 +0100

    ci: Debug ci

--- tools/scripts/upload-badge.mjs
+++ tools/scripts/upload-badge.mjs
@@ -19,10 +19,12 @@ process.chdir(join(COVERAGE, outputPath));
 const coverage = JSON.parse(readFileSync('coverage-summary.json').toString())
 const percentage = coverage['total']['statements']['pct'];
 
-const filename = name
-const content = {"schemaVersion":1,"label":"Test Coverage","message":`${percentage}%`,"color":"green","namedLogo":"jest"}
+const filename = `${name}.json`
+const content = JSON.stringify({"schemaVersion":1,"label":"Test Coverage","message":`${percentage}%`,"color":"green","namedLogo":"jest"})
 const body = JSON.stringify({ files: { [filename]: { content } } });
-console.log(body);
+
+// process.env['GIST_SECRET'] = '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'
+// process.env['GIST_ID'] = '397d521f54660656f2fd6195ec482581'
 const gistUrl = new URL(process.env['GIST_ID'], 'https://api.github.com/gists/');
 const headers = new Headers([
   ["Content-Type", "application/json"],
@@ -31,7 +33,7 @@ const headers = new Headers([
   ["Authorization", `token ${process.env['GIST_SECRET']}`],
 ]);
 
-console.log(process.env['GIST_SECRET'])
+
 
 const result = await fetch(gistUrl, {
   method: "PATCH",
