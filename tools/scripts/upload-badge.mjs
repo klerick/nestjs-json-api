28f173a30886b2e016473fbccee9a7133823dab6
Alex H <klerick666@gmail.com>
2024-03-06 17:40:55 +0100

    ci: Create task for compile for angular

--- tools/scripts/upload-badge.mjs
+++ tools/scripts/upload-badge.mjs
@@ -23,9 +23,6 @@ const filename = `${name}.json`
 const content = JSON.stringify({"schemaVersion":1,"label":"Test Coverage","message":`${percentage}%`,"color":"green","namedLogo":"jest"})
 const body = JSON.stringify({ files: { [filename]: { content } } });
 
-// process.env['GIST_SECRET'] = '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'
-// process.env['GIST_SECRET'] = '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213123123'
-// process.env['GIST_ID'] = '397d521f54660656f2fd6195ec482581'
 const gistUrl = new URL(process.env['GIST_ID'], 'https://api.github.com/gists/');
 const headers = new Headers([
   ["Content-Type", "application/json"],
