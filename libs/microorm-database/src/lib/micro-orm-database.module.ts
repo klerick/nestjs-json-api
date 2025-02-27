e3ec3802687212cfbfa34dc89156db2a63f49d5b
Alex H <klerick666@gmail.com>
2025-02-27 06:51:06 +0100

    chore: bump version
    up nx version to 24.*
    up nest version to 11.*

--- libs/microorm-database/src/lib/micro-orm-database.module.ts
+++ libs/microorm-database/src/lib/micro-orm-database.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroOrmCoreModule } from '@mikro-orm/nestjs/mikro-orm-core.module';
 
 import { config } from './config';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(config)],
-  exports: [MikroOrmModule],
+  imports: [MikroOrmModule.forRoot(config), MikroOrmModule.forMiddleware()],
+  exports: [MikroOrmCoreModule],
 })
-export class MicroOrmDatabaseModule {}
+export class MicroOrmDatabaseModule {
+  // static async forRootAsync() {
+  //   const microOrmModule = await MikroOrmModule.forRoot(config);
+  //   console.log(microOrmModule.providers);
+  //   return {
+  //     module: MicroOrmDatabaseModule,
+  //     imports: [microOrmModule],
+  //     // imports: [microOrmModule],
+  //     // exports: [MikroOrmCoreModule],
+  //   };
+  // }
+}
+
+export { config };
