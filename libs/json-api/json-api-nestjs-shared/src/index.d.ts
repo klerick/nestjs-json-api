98278b29c6e76f2fff6e0577ef6a431f875f4856
Alex H <klerick666@gmail.com>
2025-10-14 20:54:16 +0200

    chore: migrate to vitest

---
+++ libs/json-api/json-api-nestjs-shared/src/index.d.ts
@@ -0,0 +1,3 @@
+export * from './lib/utils';
+export * from './lib/types';
+export * from './lib/constants';
---
+++ libs/json-api/json-api-nestjs-shared/src/lib/constants/index.d.ts
@@ -0,0 +1,2 @@
+export declare const KEY_MAIN_INPUT_SCHEMA = "atomic:operations";
+export declare const KEY_MAIN_OUTPUT_SCHEMA = "atomic:results";
