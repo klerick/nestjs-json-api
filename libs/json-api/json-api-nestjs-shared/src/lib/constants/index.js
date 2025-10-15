afe97ba5babe653f9e7e7822eb4d58362bde8b6d
Alex H <klerick666@gmail.com>
2025-10-15 07:13:39 +0200

    chore: change tsconfig and test of type

--- libs/json-api/json-api-nestjs-shared/src/lib/constants/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KEY_MAIN_OUTPUT_SCHEMA = exports.KEY_MAIN_INPUT_SCHEMA = void 0;
-exports.KEY_MAIN_INPUT_SCHEMA = 'atomic:operations';
-exports.KEY_MAIN_OUTPUT_SCHEMA = 'atomic:results';
-//# sourceMappingURL=index.js.map
--- libs/json-api/json-api-nestjs-shared/src/lib/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './query-type';
-export * from './entity-type';
-export * from './response-body';
