a7d875a29b8b5bc9e06fb3d0187f095161c778d4
Alex H <klerick666@gmail.com>
2026-01-20 15:53:38 +0100

    feat(json-api-server): add custom postOne method in CommentsController to handle updatedAt field

--- apps/json-api-server/src/app/json-api/micro-orm/micro-orm/controllers/comments/comments.controller.ts
+++ apps/json-api-server/src/app/json-api/micro-orm/micro-orm/controllers/comments/comments.controller.ts
@@ -1,11 +1,29 @@
 import { Comments } from '@nestjs-json-api/microorm-database';
 import { InjectRepository } from '@mikro-orm/nestjs';
 
-import { Get } from '@nestjs/common';
-import { JsonApi, JsonBaseController } from '@klerick/json-api-nestjs';
+import {
+  JsonApi,
+  JsonBaseController,
+  PostData,
+  patchInputData,
+} from '@klerick/json-api-nestjs';
+import {
+  ResourceObject,
+} from '@klerick/json-api-nestjs-shared';
 import { EntityRepository } from '@mikro-orm/core';
 
 @JsonApi(Comments)
 export class CommentsController extends JsonBaseController<Comments, 'id'> {
-  @InjectRepository(Comments, 'default') commentsRepository!: EntityRepository<Comments>;
+  @InjectRepository(Comments, 'default')
+  commentsRepository!: EntityRepository<Comments>;
+
+  override postOne(
+    inputData: PostData<Comments, 'id'>
+  ): Promise<ResourceObject<Comments>> {
+    const patchedInputData = patchInputData(inputData, {
+      updatedAt: new Date(),
+    });
+
+    return super.postOne(patchedInputData);
+  }
 }
